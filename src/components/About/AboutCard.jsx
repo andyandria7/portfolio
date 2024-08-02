08b10d12c265b8edc248fcd5b1f56f8669ef11fb
Andy <andyadria7@gmail.com>
2024-08-02 21:30:00 -0100

    maj a propos

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -8,14 +8,13 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Je suis <span className="purple">ANDRIANARIMALALA Andy Anthony, développeur web spécialisé dans React et Laravel</span>.
-          J'ai obtenu une <span className="purple">licence en informatique, Risque et Décision, </span>
-            à de l'Université ESMIA à Mahamasina.
+            Titulaire d'une <span className="purple">licence en informatique, Risque et Décision</span>, de l'Université ESMIA à Mahamasina, et <span className="purple">diplômé en développement web</span> de HOPES Formation à Andavamamba Anjezika.
             <br />
-            En 2021, j'ai réalisé un stage de développeur junior à l'Association Infinity Creative, où j'ai travaillé sur un projet de développement de jeux en utilisant la programmation en C# avec les outils Unity et Krita.
+            En 2021, j'ai suivi une <span className="purple">formation en développement web chez NirInfo</span> à Ampandrana, ainsi qu'un <span className="purple">stage en tant que développeur junior à l'Association Infinity Creative</span>, où j'ai participé à un projet de <span className="purple">développement de jeux en utilisant C# avec les outils Unity et Krita.</span>
             <br />
-            En 2022, j'ai effectué un autre stage chez Hulia à Tsiadana, où j'ai contribué au développement d'un site web.
+            En 2022, j'ai réalisé un  <span className="purple">stage chez Hulia à Tsiadana</span>, contribuant activement au <span className="purple">développement d'un site web avec Laravel</span>. 
             <br />
-            Actuellement étudiant en je suis <span className="purple">étudiant en développement web chez HOPES Formation,</span> située à Andavamamba Anjezika.
+            En 2019, j'ai obtenu mon Baccalauréat série Technique à l'ESCA, Antanimena, ainsi que mon diplôme de BEP à Ampefiloha.
             <br />
             <br />
             Mes loisir!!
